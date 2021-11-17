18adec5ef38a9abcb1a4238525791c85705c3628
kdd9512 <hyunwoo8805@gmail.com>
2021-11-18 03:57:45 +0900

    20211118 1st commit
    Controller / .jsp 연결

--- src/main/java/com/springbootgradle/SpringBootGradleApplication.java
+++ src/main/java/com/springbootgradle/SpringBootGradleApplication.java
@@ -2,12 +2,15 @@ package com.springbootgradle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.springbootgradle.controller")
 public class SpringBootGradleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootGradleApplication.class, args);
+        System.out.println("Initializing...");
     }
 
 }
