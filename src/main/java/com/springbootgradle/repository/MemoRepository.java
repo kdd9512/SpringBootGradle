dd293da25f40ade39ce75cf85f8cf65f62f45ce0
kdd9512 <hyunwoo8805@gmail.com>
2021-11-23 20:00:25 +0900

    20211123 3nd commit
    JpaRepository 를 이용하여 DB 에 CRUD 적용.
    @Query Annotation 사용법.
    ThymeLeaf start

--- src/main/java/com/springbootgradle/repository/MemoRepository.java
+++ src/main/java/com/springbootgradle/repository/MemoRepository.java
@@ -1,9 +1,44 @@
 package com.springbootgradle.repository;
 
 import com.springbootgradle.entity.Memo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 // interface JpaRepository <@Entity 의 type 정보(여기서는 Memo class), @Id 의 type(private "Long" mno)>
 public interface MemoRepository extends JpaRepository<Memo, Long> {
+
+    // findBy(찾는조건)Between ? OrderBy(정렬할조건) Desc(내림) / Asc(오름)
+    // Between ? : ( Long from(시작점), Long to(종료점) );
+    List<Memo> findByMnoBetweenOrderByMnoDesc(Long from, Long to);
+
+    // 위에서 사용한대로 사용해도 무관하나 Page<> 를 이용하여 단축할 수 있는 방법이 있다.
+    Page<Memo> findByMnoBetween(Long from, Long to, Pageable pageable);
+
+    // 컬럼 삭제를 위한 QUERY 문.
+//    void deleteMemoByMnoLessThan(Long num);
+
+    // JPQL 문.
+    // select DB별칭 from @Entity이름 (as는 생략가능) DB 별칭 order by 별칭.기준 정렬기준(오름/내림)
+    @Query("select asdf from Memo as asdf order by asdf.mno desc")
+    List<Memo> getListDesc();
+
+//    @Transactional
+//    @Modifying
+//    @Query("update Memo as asdf set asdf.memoText = :memoText where asdf.mno = :mno ")
+//    int updateMemoText(@Param("mno") Long mno, @Param("memoText") String memoText);
+
+    // updateMemoText 를 #{} 이용하여 간략화(jquery 에서 쓰던 그거. 자바 Bean 스타일을 활용).
+    // Query 문이 지나치게 길어지는 것을 방지하기 위하여 사용한다.
+    @Transactional
+    @Modifying
+    @Query("update Memo as asdf set asdf.memoText = :#{#param.memoText} where asdf.mno = :#{#param.mno}")
+    int updateMemoText(@Param("param") Memo memo);
 
 }
--- src/test/java/com/springbootgradle/repository/MemoRepositoryTests.java
+++ src/test/java/com/springbootgradle/repository/MemoRepositoryTests.java
@@ -4,8 +4,14 @@ import com.springbootgradle.entity.Memo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -44,7 +50,7 @@ public class MemoRepositoryTests {
 //        }
 //    }
 
-    // find(using getOne())
+    // find(using getOne()) ** deprecate 된 기능.
     // getOne 을 사용하려면 Transactional annotation 이 필요하다
     // (트랜잭션 처리가 필요함.)
     // 실제 객체가 사용되는 순간에 SQL 문이 작동한다.
@@ -79,12 +85,120 @@ public class MemoRepositoryTests {
     // select 문으로 해당 컬럼의 존재여부 확인.
     // (없는 경우, EmptyResultDataAccessException 발생.)
     // 해당 조건에 맞는 컬럼 삭제.
-    @Test
-    public void testDelete(){
-        Long mno = 99L;
+//    @Test
+//    public void testDelete(){
+//        Long mno = 99L;
+//
+//        memoRepository.deleteById(mno);
+//
+//    }
 
-        memoRepository.deleteById(mno);
 
-    }
+    // 페이징 처리(p66).
+    // Pageable 인터페이스를 사용한다.
+    // Pageable 은 페이지 처리에 필요한 정보를 전달하는 역할을 한다.
+    // 실제 객체를 구성할 때 에는 PageRequest 를 이용한다.
+//    @Test
+//    public void testPageDefault() {
+    // PageRequest 는 protected 로 선언되어 있어 new 를 이용할 수 없기 때문에
+    // 예시와 같이 .of() 를 이용해서 처리해야 한다.
+
+    // 1 페이지 당 10개
+//        Pageable pageable = PageRequest.of(0, 10);
+//        Page<Memo> result = memoRepository.findAll(pageable);
+//        System.out.println(result);
+//
+//        System.out.println("==============================================");
+//
+//        // 총 페이지 수.
+//        System.out.println("Total Pages : " + result.getTotalPages());
+//
+//        // 총 컬럼 개수.
+//        System.out.println("Total Elements : " + result.getTotalElements());
+//
+//        // 현재 페이지 번호(0부터 시작함).
+//        System.out.println("Current Pages : " + result.getNumber());
+//
+//        // 페이지 당 데이터 개수.
+//        System.out.println("Page Size : " + result.getSize());
+//
+//        // 다음 페이지 존재여부
+//        System.out.println("has next page ? : " + result.hasNext());
+//
+//        // 시작페이지(0 페이지) 여부.
+//        System.out.println("is first page? : " + result.isFirst());
+//
+//        System.out.println("==============================================");
+//
+//        // 데이터 내용 추출.
+//        for (Memo memo : result.getContent()) {
+//            System.out.println(memo);
+//        }
+//    }
+
+//    @Test
+//    public void testSort() {
+//
+//        // .by("정렬기준")
+//        Sort srt1 = Sort.by("mno").descending();
+//        Sort srt2 = Sort.by("memoText").ascending();
+//
+//        // .and 를 이용하여 두 Sort 조건을 연결.
+//        Sort srtAll = srt1.and(srt2);
+//
+//        Pageable pageable = PageRequest.of(0, 10, srtAll);
+//
+//        Page<Memo> result = memoRepository.findAll(pageable);
+//
+//        result.get().forEach(memo -> {
+//            System.out.println(memo);
+//        });
+//    }
+
+
+    // Query 메서드
+//    @Test
+//    public void testQueryMethods() {
+//        List<Memo> list = memoRepository.
+//        findByMnoBetweenOrderByMnoDesc(70L, 80L);
+//
+//        for (Memo memo : list) {
+//            System.out.println(memo);
+//        }
+//    }
+
+    // Query 문 + Pageable(PageRequest) 혼합.
+//    @Test
+//    public void testQueryMethodsWithPageable(){
+//        // 기존 Query 메서드명에 OrderBy(조건) 와 Desc 를 작성하여 처리했던 부분을
+//        // PageRequest.of() 에 담아 처리.
+//        Pageable pageable = PageRequest.of(0,10,Sort.by("mno").descending());
+//
+//        Page<Memo> result = memoRepository.findByMnoBetween(10L, 50L, pageable);
+//
+//        // forEach 문으로 조건에 맞는 컬럼 전부출력.
+//        result.get().forEach(memo -> {
+//            System.out.println(memo);
+//        });
+//    }
+
+//    // @Commit : 실제 DB 에 적용시키기 위한 Annotation.
+//    @Commit
+//    // @Transactional :
+//    // 먼저 해당 값이 있는지 여부를 검색해야 할 때 반드시 필요한 Annotation.
+//    @Transactional
+//    @Test
+//    public void testDeleteQueryMethods() {
+//        memoRepository.deleteMemoByMnoLessThan(5L);
+//    }
+
+//    @Test
+//    public void annotationQueryTest(){
+//        List<Memo> list = memoRepository.getListDesc();
+//
+//        for (Memo memo : list) {
+//            System.out.println(memo);
+//        }
+//    }
 
 }
